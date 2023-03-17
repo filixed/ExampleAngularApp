8d5278ec4a41ccce7461d42f6a266e814510c0ea
filip.breska <filixed@gmail.com>
2023-03-17 15:32:14 +0100

    feat: basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './Components/layout/layout.component';
+import { TableComponent } from './Components/table/table.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    TableComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
